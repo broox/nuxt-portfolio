886cbd81ad60f84d989a5d26aa0265b2b33fde90
Derek Brooks <derek@broox.com>
2022-08-26 16:14:58 -0500

    fix up browser head config for nuxt2. add favicon.

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,20 +12,30 @@ export default {
   generate: {
     fallback: '404.html',
   },
-  htmlAttrs: {
-    lang: 'en',
+  head: {
+    htmlAttrs: {
+      lang: 'en',
+    },
+    link: [
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#ffffff' },
+      { rel: 'manifest', href: '/site.webmanifest' },
+    ],
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no' },
+      { hid: 'author', name: 'author', content: 'Derek Brooks' },
+      { name: 'format-detection', content: 'telephone=no' },
+      { property: 'og:site_name', content: 'The Portfolio of Derek Brooks' },
+      { name: 'twitter:creator', content: '@broox' },
+      { name: 'twitter:site', content: '@broox' },
+      { name: 'msapplication-TileColor', content:'#2563eb'},
+      { name: 'theme-color', content: '#ffffff' },
+    ],
+    title: 'Derek Brooks\'s Portfolio'
   },
-  meta: [
-    { charset: 'utf-8' },
-    { name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no' },
-    { hid: 'author', name: 'author', content: 'Derek Brooks' },
-    { name: 'format-detection', content: 'telephone=no' },
-    { property: 'og:site_name', content: 'The Portfolio of Derek Brooks' },
-    { name: 'twitter:creator', content: '@broox' },
-    { name: 'twitter:site', content: '@broox' },
-    { name: 'msapplication-TileColor', content:'#2563eb'},
-    { name: 'theme-color', content: '#ffffff' },
-  ],
   plugins: [
     '~/plugins/dates',
   ],
--- pages/404.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <header class="title">
-    <h1>Page not found</h1>
-    <p>Whoops, you've stumbled upon a link that doesn't exist... 😬</p>
-    <p>Why don't you check out my <NuxtLink to="/">featured projects</NuxtLink> instead? 😁</p>
-  </header>
-</template>
